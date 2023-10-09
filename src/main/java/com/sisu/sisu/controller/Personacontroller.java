79fb1c40390a4742fcd062788be551fccf0ed1a5
javierfc16 <javierfc1611@gmail.com>
2023-10-09 17:58:36 -0400

    FDG

--- src/main/java/com/sisu/sisu/controller/Personacontroller.java
+++ src/main/java/com/sisu/sisu/controller/Personacontroller.java
@@ -17,7 +17,7 @@ public class Personacontroller {
     @Autowired
     private IPersonaService personaService;
     
-    @GetMapping(value = "/")
+    @GetMapping(value = "/p")
     public String registroPersona(@Validated Persona persona, Model model){
         
         model.addAttribute("persona", new Persona());
