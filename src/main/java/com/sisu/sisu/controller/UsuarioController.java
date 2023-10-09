4b8f1cba4774bfefa77ead8774b3effab4c1771b
javierfc16 <javierfc1611@gmail.com>
2023-10-09 11:13:21 -0400

    upd J3

--- src/main/java/com/sisu/sisu/controller/UsuarioController.java
+++ src/main/java/com/sisu/sisu/controller/UsuarioController.java
@@ -1,8 +1,62 @@
 package com.sisu.sisu.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.sisu.sisu.Service.UsuarioService;
+import com.sisu.sisu.entitys.Usuarios;
 
 @Controller
 public class UsuarioController {
+
+    @Autowired
+    private UsuarioService usuarioService;
+
+    @GetMapping(value="/formUs")
+    public String vistaUs(Model model) {
+
+        model.addAttribute("usuario", new Usuarios());
+        model.addAttribute("usuarios", usuarioService.findAll());
+
+        return "usuarios";
+    }
+
+
+    @PostMapping(value="/saveUs")
+    public String saveUsiario(@Validated Usuarios usuarios) {
+
+        usuarios.setEstado("A");
+
+        usuarioService.save(usuarios);
+
+        return "";
+    }
+
+    @GetMapping(value="/listaUs")
+    public String listaUs(Model model) {
+
+        model.addAttribute("usuarios", usuarioService.findAll());
+
+        return "usuarios";
     
+    }
+
+    @GetMapping(value="/eliminarUs/{idUsuario}")
+    public String deleteUs(@PathVariable("idUsuario") Integer idUsuario) {
+
+        Usuarios usuario = usuarioService.findOne(idUsuario);
+
+        usuario.setEstado("X");
+
+        usuarioService.save(usuario);
+
+        return "";
+
+    }
+
 }
