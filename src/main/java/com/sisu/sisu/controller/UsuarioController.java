dcb0e300c36ed1139f94ceac2ddc9e9beac8f6ba
javierfc16 <javierfc1611@gmail.com>
2023-10-11 17:02:55 -0400

    11-10-2023-17:02

--- src/main/java/com/sisu/sisu/controller/UsuarioController.java
+++ src/main/java/com/sisu/sisu/controller/UsuarioController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import com.sisu.sisu.Dao.UsuarioDao;
 import com.sisu.sisu.Service.IPersonaService;
 import com.sisu.sisu.Service.UsuarioService;
 import com.sisu.sisu.entitys.Persona;
